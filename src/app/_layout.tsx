972e368e9d31b83f70871c9fe03e3fd59ec4e0af
BlinkyPanoptes <cassey1226@gmail.com>
2026-05-22 14:45:33 +0800

    Initial Expo React Native setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,9 +3,11 @@ import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
+import '../global.css';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
